08bdd8114e042551b0e869102cd82286ea313bb3
Khiruthiha MS <156287797+krxthxx22@users.noreply.github.com>
2025-05-08 07:59:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Union of 2 Sorted with Duplicates/union-of-2-sorted-with-duplicates.cpp
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // a,b : the arrays
+    // Function to return a list containing the union of the two arrays.
+    vector<int> findUnion(vector<int> &a, vector<int> &b) {
+        // Your code here
+        vector<int> Union;
+        map<int,int> freq;
+        
+        int n = a.size();
+        int m = b.size();
+        
+        for(int i =0;i<n;i++)
+        {
+            freq[a[i]]++;
+        }
+        
+        for(int i = 0;i<m;i++)
+        {
+            freq[b[i]]++;
+        }
+        
+        for (auto & it: freq)
+          Union.push_back(it.first);
+        return Union;
+        // return vector with correct order of elements
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> a, b;
+        string input;
+
+        // Read first array
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            a.push_back(number);
+        }
+
+        // Read second array
+        getline(cin, input);
+        stringstream ss2(input);
+        while (ss2 >> number) {
+            b.push_back(number);
+        }
+
+        Solution ob;
+        vector<int> ans = ob.findUnion(a, b);
+        for (int i : ans)
+            cout << i << ' ';
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
